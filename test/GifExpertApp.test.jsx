27fa3b4930a5f97d9917c7a1567af533c8147382
Jossue <jativatamayojossuedavid@gmail.com>
2024-05-10 09:56:49 -0500

    Ingreso de mas informacion

--- test/GifExpertApp.test.jsx
+++ test/GifExpertApp.test.jsx
@@ -13,4 +13,32 @@ describe('Realizar el test de <GifExpertApp />', () => {
         expect(screen.getByText(categories[0])).toBeInTheDocument();
         expect(screen.getByText(categories[1])).toBeInTheDocument();
     });
+
+    test('Debe de mostrar un input para agregar una categoría', () => {
+        render(<GifExpertApp />);
+        expect(screen.getByRole('textbox')).toBeInTheDocument();
+    });
+
+    test('Debe de mostrar un botón para agregar una categoría', () => {
+        render(<GifExpertApp />);
+        expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+
+    test('Debe de mostrar un mensaje de error si se intenta agregar una categoría vacía', () => {
+        render(<GifExpertApp />);
+        const input = screen.getByRole('textbox');
+        const button = screen.getByRole('button');
+        input.value = '';
+        button.click();
+        expect(screen.getByText('El campo no puede estar vacío')).toBeInTheDocument();
+    });
+
+    test('Debe de agregar una categoría al hacer click en el botón', () => {
+        render(<GifExpertApp />);
+        const input = screen.getByRole('textbox');
+        const button = screen.getByRole('button');
+        input.value = 'Naruto';
+        button.click();
+        expect(screen.getByText('Naruto')).toBeInTheDocument();
+    });
 });
